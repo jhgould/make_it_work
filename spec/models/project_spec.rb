ec4a2a3eed8cd27e380605c0a0e398bf8e31e38c
Jack Gould <johnhilsmangould@gmail.com>
2020-05-15 10:06:27 -0600

    add avg model test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  it "can average" do
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+    contestant_collection = Contestant.all
+
+
+    expect(contestant_collection.avg_exp).to eq(12.5)
+
+  end
 end
