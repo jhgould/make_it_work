b0eacc7fe6cf81dbc1b359369e96718f460dff0c
Jack Gould <johnhilsmangould@gmail.com>
2020-05-15 09:26:17 -0600

    add show page for user story 1

--- spec/features/project/project_show_spec.rb
+++ spec/features/project/project_show_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe "project show page", type: :feature do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
-    @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    @boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-    @upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-    @lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
     @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
     @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
@@ -21,10 +21,9 @@ RSpec.describe "project show page", type: :feature do
 
   it "can access show page and see projects name, material and theme" do
     visit "/projects/#{@news_chic.id}"
-
     expect(page).to have_content(@news_chic.name)
     expect(page).to have_content(@news_chic.material)
-    expect(page).to have_content(@news_chic.theme)
+    expect(page).to have_content(@recycled_material_challenge.theme)
 
 
 
