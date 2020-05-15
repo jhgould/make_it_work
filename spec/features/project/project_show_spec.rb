b932e2e0553dfe73aee8d8296f44084211706488
Jack Gould <johnhilsmangould@gmail.com>
2020-05-15 10:22:30 -0600

    add more robust testing by making expect more specific

--- spec/features/project/project_show_spec.rb
+++ spec/features/project/project_show_spec.rb
@@ -38,24 +38,24 @@ RSpec.describe "project show page", type: :feature do
 
 
     visit "/projects/#{@news_chic.id}"
-    expect(page).to have_content(@news_chic.name)
-    expect(page).to have_content(@news_chic.material)
-    expect(page).to have_content(@recycled_material_challenge.theme)
+    expect(page).to have_content("Name: #{@news_chic.name}")
+    expect(page).to have_content("Material: #{@news_chic.material}")
+    expect(page).to have_content("Theme: #{@recycled_material_challenge.theme}")
     expect(page).to have_content("Number Of Contestants: 2")
 
     visit "/projects/#{@upholstery_tux.id}"
-    expect(page).to have_content(@upholstery_tux.name)
-    expect(page).to have_content(@upholstery_tux.material)
-    expect(page).to have_content(@furniture_challenge.theme)
+    expect(page).to have_content("Name: #{@upholstery_tux.name}")
+    expect(page).to have_content("Material: #{@upholstery_tux.material}")
+    expect(page).to have_content("Theme: #{@furniture_challenge.theme}")
     expect(page).to have_content("Number Of Contestants: 2")
 
   end
   it "user can see avg years of experience" do
 
     visit "/projects/#{@news_chic.id}"
-    expect(page).to have_content(@news_chic.name)
-    expect(page).to have_content(@news_chic.material)
-    expect(page).to have_content(@recycled_material_challenge.theme)
+    expect(page).to have_content("Name: #{@news_chic.name}")
+    expect(page).to have_content("Material: #{@news_chic.material}")
+    expect(page).to have_content("Theme: #{@recycled_material_challenge.theme}")
     expect(page).to have_content("Number Of Contestants: 2")
     expect(page).to have_content("Average Contestant Experience: 12.5")
 
